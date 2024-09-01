67585851a0c52b34831c982e8223f0898000cbb3
Alessio Sferro <sferro.alessio@gmail.com>
2024-09-01 20:30:38 +0200

    feat: add confirm shortcut change modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
-import React, {FormEventHandler, useEffect, useMemo, useState} from 'react';
+import React, {
+  FocusEventHandler,
+  FormEventHandler,
+  KeyboardEventHandler,
+  useEffect,
+  useMemo,
+  useRef,
+  useState
+} from 'react';
 import TOOL_MAP, {Tool} from "./utils/tool-map";
-import {Box, Button, ChakraBaseProvider, Flex, FormControl, FormLabel, Heading, Input, Text} from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  ChakraBaseProvider,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text
+} from "@chakra-ui/react";
 import theme from "./theme";
 import LOCAL_STORAGE_SHORTCUTS_KEY from "./utils/local-storage-shortcuts-key";
 import defaultToolShortcuts from "./utils/default-tool-shortcuts";
@@ -8,8 +32,11 @@ import defaultToolShortcuts from "./utils/default-tool-shortcuts";
 
 export default function App() {
   const savedShortcuts = localStorage.getItem(LOCAL_STORAGE_SHORTCUTS_KEY);
-  const [storedShortcuts, setStoredShortcuts] = useState(savedShortcuts ? JSON.parse(savedShortcuts) : defaultToolShortcuts);
+  const [storedShortcuts, setStoredShortcuts] = useState<Record<string, string>>(savedShortcuts ? JSON.parse(savedShortcuts) : defaultToolShortcuts);
   const [filterKey, setFilterKey] = useState("");
+  const [duplicateToolName, setDuplicateToolName] = useState("");
+
+  const refs = useRef<{ toolName: string, el: HTMLDivElement }[]>([]);
 
   const filteredShortcuts = useMemo(() => Object.entries(storedShortcuts).filter(([toolName]) =>
     toolName.toLowerCase().includes(filterKey.toLowerCase()))
@@ -50,8 +77,62 @@ export default function App() {
     location.reload();
   }
 
+  const handleKeyChange: KeyboardEventHandler<HTMLInputElement> = (event) => {
+    const value = [];
+
+    event.preventDefault();
+
+    if (event.shiftKey) {
+      value.push("Shift");
+    }
+
+    if (event.altKey) {
+      value.push("Alt")
+    }
+
+    if (!["Alt", "Shift"].includes(event.key)) {
+      value.push(event.code.toLowerCase().slice(-1))
+    }
+
+    event.currentTarget.value = value.join('+');
+  }
+
+  const handleBlur: FocusEventHandler<HTMLInputElement> = (event) => {
+    const storedShortcut = Object.entries(storedShortcuts).find(([, shortcut]) => shortcut === event.currentTarget.value);
+    if (!storedShortcut) return;
+    const [toolName] = storedShortcut;
+    setDuplicateToolName(toolName);
+  }
+
+  const closeModal = () => {
+    setDuplicateToolName("");
+  }
+
   return (
     <ChakraBaseProvider theme={theme}>
+      <Modal onClose={closeModal} isOpen={duplicateToolName !== ""}>
+        <ModalOverlay/>
+        <ModalContent>
+          <ModalHeader>Confirm new shortcut</ModalHeader>
+          <ModalBody>
+            The shortcut entered is already assigned to {duplicateToolName}. Are you sure you want to reset
+            the {duplicateToolName} shortcut?
+          </ModalBody>
+          <ModalFooter>
+            <Button>Abort</Button>
+            <Button onClick={() => {
+              const element = (refs.current.find(ref => ref.toolName)?.el as HTMLInputElement);
+
+              if (!element) return;
+
+              element.value = "";
+
+              closeModal()
+            }}>Confirm</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
       <Box m="1rem 2rem">
         <Heading mb="1rem" fontSize="2.4rem" as="h1">Geogebra Shortcuts Extension 1.0</Heading>
 
@@ -69,10 +150,11 @@ export default function App() {
         <Box as="form" onSubmit={handleSave}>
           <Flex direction="column" gap="2rem">
             {Object.entries(filteredShortcuts).map(([toolName, {shortcut, description}]) => (
-              <Flex key={toolName} as="label" direction="column">
+              <Flex ref={(el) => el && refs.current.push({el, toolName})} key={toolName} as="label"
+                    direction="column">
                 <Heading mb="1rem">{toolName}</Heading>
                 <Text mb=".8rem">{description}</Text>
-                <Input name={toolName} defaultValue={shortcut}/>
+                <Input onBlur={handleBlur} onKeyDown={handleKeyChange} name={toolName} defaultValue={shortcut}/>
               </Flex>
             ))}
           </Flex>
